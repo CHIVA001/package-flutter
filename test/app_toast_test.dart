9dced135973e675f84610e5884d0fd76cfdcf319
CHIVA001 <chivathe1@gmail.com>
2026-05-29 14:10:48 +0700

    Release version 1.0.1

--- test/app_toast_test.dart
+++ test/app_toast_test.dart
@@ -1,6 +1,6 @@
 // test/app_toast_test.dart
 
-import 'package:app_toast/app_toast.dart';
+import 'package:glass_app_toast/app_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
